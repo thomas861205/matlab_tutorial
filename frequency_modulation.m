e7bbe3419cdad0c98c5383d64c490e43deba9697
Thomas <thomaschou861205@gmail.com>
2018-07-07 03:16:49 +0800

    add frequency modulation

--- frequency_modulation.m
+++ frequency_modulation.m
@@ -5,14 +5,14 @@ Ac = 1;
 fc = 100;
 
 % modulation frequency
-fm  = 5;
+fm  = 8;
 
 % modulation index
-m = 2;
+m = 5;
 
-L = 200;
-t = (0: L-1)/fc;
+L = 1000;
+t = linspace(0, 1, L);
 y = Ac*cos(2*pi*fc*t - m*sin(2*pi*fm*t) );
 
 plot(t, y);
-%axis([0, L/fc, 0, 2]);
+axis([0, 0.5, -2, 2]);
